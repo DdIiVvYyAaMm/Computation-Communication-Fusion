e23817ac2978e3d28e1adb2e56dedba61187c52d
Peter Cao <peterspellingbee@gmail.com>
2024-11-28 01:01:58 -0500

    Add python runner scripts

--- basic_cuda/run.py
+++ basic_cuda/run.py
@@ -2,10 +2,10 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 
 # Load the CUBIN
-module = cuda.module_from_file("main.cubin")
+module = cuda.module_from_file("new_main.cubin")
 
 # Get the kernel function
-kernel = module.get_function("helloWorldKernel")
+kernel = module.get_function("_Z16helloWorldKernelv")
 
 # Launch the kernel
 kernel(block=(256, 1, 1), grid=(1, 10))
