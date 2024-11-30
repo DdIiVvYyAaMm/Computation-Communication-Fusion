c9de4df7354b2fddab30ecb5c9916ebe4baad5af
Harsh-Sinha <harsh.sinha@hotmail.com>
2024-11-30 16:32:14 -0500

    add build command to generate llvm ir for cuda kernel

--- comp_comm_kernel/build.sh
+++ comp_comm_kernel/build.sh
@@ -2,12 +2,15 @@
 clang++ $1.cu -S -x cuda -I/usr/include/c++/11/ -I/usr/include/x86_64-linux-gnu/c++/11/ \
         -emit-llvm -O0 -Xclang -disable-llvm-passes --cuda-gpu-arch=sm_89 
 
-# Step 2: Convert LLVM IR to PTX Assembly
-llc -march=nvptx64 -mcpu=sm_89  $1-cuda-nvptx64-nvidia-cuda-sm_89.bc -o $1.ptx
+# Step 2: Convert LLVM BC to LLVM IR
+llvm-dis $1-cuda-nvptx64-nvidia-cuda-sm_89.bc -o $1.ll
 
-# Step 3: Check the PTX file for errors
+# Step 3: Convert LLVM IR to PTX Assembly
+llc -march=nvptx64 -mcpu=sm_89  $1.ll -o $1.ptx
+
+# Step 4: Check the PTX file for errors
 echo "Checking for errors in PTX file..."
 ptxas -arch=sm_89 -o /dev/null $1.ptx
 
-# Step 4: Generate CUBIN from PTX
+# Step 5: Generate CUBIN from PTX
 nvcc -arch=sm_89 -cubin -o $1_gemm.cubin $1.ptx
