b09c695d4c3c94da0096b7e70ea2e396101da58e
Harsh-Sinha <harsh.sinha@hotmail.com>
2024-12-02 16:37:36 -0500

    get rid of build folder for loop tiling pass

--- comp_comm_kernel/build/LoopTilingPass/CMakeFiles/LoopTilingPass.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LoopTilingPass.
-# This may be replaced when dependencies are built.
--- comp_comm_kernel/build/LoopTilingPass/CMakeFiles/install-LoopTilingPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LoopTilingPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LoopTilingPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
